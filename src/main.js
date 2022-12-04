3bae6bc27b2a94ff472d64196662dd6c19abda5a
Eugene <effectorea@yandex.ru>
2022-12-04 14:32:50 +0300

    in process of realising localization

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import currencyFilter from "@/filters/currency.filter";
+import localizeFilter from "@/filters/localize.filter";
 
 Vue.config.productionTip = false;
 
@@ -22,6 +23,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
 Vue.directive('tooltip', directiveTooltip)
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,18 +2,31 @@
   <div>
     <div>
       <div class="page-title">
-        <h3>Профиль</h3>
+        <h3>{{'profileTitle' | localize}}</h3>
       </div>
 
-      <form class="form">
+      <form class="form" @submit.prevent="submitHandler">
         <div class="input-field">
           <input
+            v-model="name"
+            :class="{invalid: ($v.name.$dirty && !$v.name.required)}"
             id="description"
             type="text"
           >
           <label for="description">Имя</label>
           <span
-            class="helper-text invalid">name</span>
+            v-if="$v.name.$dirty && !$v.name.required"
+            class="helper-text invalid">Поле не может быть пустым</span>
+        </div>
+
+        <div class="switch">
+          <label>
+            English
+            <input type="checkbox"
+            v-model="isRuLocale"/>
+            <span class="lever"></span>
+            Русский
+          </label>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -24,3 +37,53 @@
     </div>
   </div>
 </template>
+
+<script>
+import {mapGetters, mapActions} from "vuex";
+import { required } from "vuelidate/lib/validators";
+
+export default {
+  data: () => ({
+    name: '',
+    isRuLocale: true
+  }),
+  validations: {
+    name: {required},
+  },
+  mounted() {
+    this.isRuLocale = this.info.locale === 'ru-RU'
+    this.name = this.info.name
+    setTimeout(() => {
+      M.updateTextFields()
+    }, 0)
+  },
+  computed: {
+    ...mapGetters(['info'])
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? 'ru-RU' : 'en-US'
+        })
+      } catch (e) {
+        
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.switch {
+  margin-bottom: 2rem;
+}
+
+</style>
